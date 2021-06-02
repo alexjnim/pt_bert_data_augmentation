c199922036098463cb4bdec73cecc4d26abeb739
Alex Nim <alexnim@rol-anim.lan>
2021-06-02 18:30:47 +0100

    changing percentage to augment param

--- utils/log_config_params.py
+++ utils/log_config_params.py
@@ -6,7 +6,7 @@ def log_config_params():
 
     mlflow.log_param("reduc_factor", config.reduce_factor)
     mlflow.log_param("top_categories", config.top_categories)
-    mlflow.log_param("n_sentences_percent", config.n_sentences_percent)
+    mlflow.log_param("percent_to_augment", config.percent_to_augment)
     mlflow.log_param("new_sent_per_sent", config.new_sent_per_sent)
     mlflow.log_param("num_words_replace", config.num_words_replace)
     mlflow.log_param("vectorizer_type", config.vectorizer_type)
