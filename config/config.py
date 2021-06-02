3768067e7c13ef6200fd15dfd50b012627d9670e
Alex Nim <alexnim@rol-anim.lan>
2021-06-02 15:58:42 +0100

    addign file_path

--- config/config.py
+++ config/config.py
@@ -1,9 +1,20 @@
 reduce_factor = 0.1
 top_categories = 5
-
 # all if you want to select all
 n_sentences_percent = 0.5
 new_sent_per_sent = 1
 num_words_replace = 3
+
+file_path = (
+    "data/augmented_data_rf_"
+    + str(reduce_factor)
+    + "_nsps_"
+    + str(new_sent_per_sent)
+    + "_nwr_"
+    + str(num_words_replace)
+    + ".csv"
+)
+
+
 # tfidf, word2vec, BERT, sentencetransformer
 vectorizer_type = "fasttext"
