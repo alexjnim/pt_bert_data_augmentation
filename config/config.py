97feb739361faeb52cbf9abaa2928575be4d3580
Alex Nim <alexnim@rol-anim.lan>
2021-06-11 16:13:25 +0100

    changing the filepaths with pathlib

--- config/config.py
+++ config/config.py
@@ -1,3 +1,6 @@
+import path
+import pathlib
+
 reduce_factor = 0.2
 top_categories = 5
 test_size = 0.2
@@ -9,9 +12,8 @@ num_words_replace = 3
 # tfidf, word2vec, fastttext, BERT, sentencetransformer
 vectorizer_type = "tfidf"
 
-
-aug_file_path = (
-    "data/augmented_train_data_pta_"
+aug_file_name = (
+    "augmented_train_data_pta_"
     + str(percent_to_augment)
     + "_rf_"
     + str(reduce_factor)
@@ -24,8 +26,8 @@ aug_file_path = (
     + ".csv"
 )
 
-test_file_path = (
-    "data/test_data_rf_"
+test_file_name = (
+    "test_data_rf_"
     + str(reduce_factor)
     + "_nsps_"
     + str(new_sent_per_sent)
@@ -36,8 +38,8 @@ test_file_path = (
     + ".csv"
 )
 
-train_file_path = (
-    "data/train_data_rf_"
+train_file_name = (
+    "train_data_rf_"
     + str(reduce_factor)
     + "_nsps_"
     + str(new_sent_per_sent)
@@ -47,3 +49,10 @@ train_file_path = (
     + str(test_size)
     + ".csv"
 )
+
+PROJECT_PATH = pathlib.Path(path.__file__).resolve().parent
+DATA_DIR = "data"
+
+aug_file_path = PROJECT_PATH / DATA_DIR / aug_file_name
+test_file_path = PROJECT_PATH / DATA_DIR / test_file_name
+train_file_path = PROJECT_PATH / DATA_DIR / train_file_name
